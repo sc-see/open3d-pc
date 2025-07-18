5ff7e4bb80fa5866d452b50d8f2aafc36231ae84
sc-see <161543353+sc-see@users.noreply.github.com>
2025-07-19 01:57:12 +0800

    test: update dummy points in downsample test

--- tests/test_point_cloud_preprocessor.py
+++ tests/test_point_cloud_preprocessor.py
@@ -6,7 +6,9 @@ from src.open3d_pc.point_cloud_preprocessor import PointCloudPreprocessor
 
 def test_downsample():
     preprocesser = PointCloudPreprocessor()
-    points = np.random.rand(100, 3)
+    points = np.random.rand(500, 3)
     pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points))
     downsampled_pcd = preprocesser.downsample(pcd)
+    
+    assert isinstance(downsampled_pcd, o3d.geometry.PointCloud)
     assert len(downsampled_pcd.points) < len(pcd.points)
