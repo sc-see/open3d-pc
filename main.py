a9d69c40e1d62bfd41ec2d1103ebba18c51675de
sc-see <161543353+sc-see@users.noreply.github.com>
2025-07-21 12:17:20 +0800

    chore: add Hydra setup with base config file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 
+import hydra
 import matplotlib.pyplot as plt
 import open3d as o3d
+from omegaconf import DictConfig
 
 from src.open3d_pc.logging_config import setup_logging
 from src.open3d_pc.point_cloud_loader import PointCloudLoader
@@ -11,18 +13,19 @@ from src.open3d_pc.point_cloud_clusterer import PointCloudClusterer
 logger = logging.getLogger(__name__)
 
 
-def main():
-    setup_logging(logging.INFO)
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    setup_logging()
     
-    loader = PointCloudLoader()
+    loader = PointCloudLoader(cfg.loader.path)
     pcd = loader.load()
     logger.info(f"Loaded point cloud with {len(pcd.points)} points.")
     
-    preprocessor = PointCloudPreprocessor()
+    preprocessor = PointCloudPreprocessor(**cfg.preprocessor)
     processed_pcd = preprocessor.run(pcd)
     logger.info(f"Processed point cloud with {len(processed_pcd.points)} points.")
 
-    clusterer = PointCloudClusterer()
+    clusterer = PointCloudClusterer(**cfg.clusterer)
     labels, clustered_pcd = clusterer.cluster(processed_pcd)
     logger.info(f"Clustered point cloud into {labels.max() + 1} clusters.")
 
